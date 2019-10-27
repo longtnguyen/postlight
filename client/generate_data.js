5063b435d8dbc544f2e4199917d66208919db5c6
Long <caycham4801@gmail.com>
2019-10-27 18:12:49 -0400

    move react out of devdep, add generate test data script

---
+++ client/generate_data.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const fs = require('fs');
+axios.get('https://randomuser.me/api/?results=500&nat=us')
+  .then(function (response) {
+    fs.writeFile('testData.json', JSON.stringify(response.data.results), null, 2);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
