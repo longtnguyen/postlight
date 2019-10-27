82630d1876dcc45e3a62da2920476f9762ed2050
Long <caycham4801@gmail.com>
2019-10-27 00:00:54 -0400

    add test, git ignoring coverage

---
+++ client/src/test/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from '../components/App';
+
+import { shallow } from 'enzyme';
+import { exportAllDeclaration } from '@babel/types';
+
+describe('render', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  });
+  it('should render a header', () => {
+    const head = wrapper.find('header.App-header');
+    expect(head.length).toEqual(1);
+  });
+  it('should have a title', () => {
+    const title = wrapper.find('h1.App-title');
+    expect(title.length).toEqual(1);
+  });
+  it('should render 5 routes', () => {
+    const routes = wrapper.find('Route');
+    expect(routes.length).toEqual(5);
+
+  });
+
+})
